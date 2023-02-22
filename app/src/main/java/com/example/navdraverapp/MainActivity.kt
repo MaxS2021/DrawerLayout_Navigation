1cb51f425c165e9b1c87eabde3008863773255ad
MaxS2021 <maxs2021git@gmail.com>
2023-02-22 18:18:01 +0300

    2 Commit

--- app/src/main/java/com/example/navdraverapp/MainActivity.kt
+++ app/src/main/java/com/example/navdraverapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.navdraverapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.navdraverapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var conf: AppBarConfiguration
+    lateinit var navController: NavController
+    lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setSupportActionBar(bind.aBar.toolbar)
+        //setupActionBarWithNavController(navController)
+        conf = AppBarConfiguration(
+            setOf(
+                R.id.item1,
+                R.id.item2
+            ), bind.drawer
+        )
+        setupActionBarWithNavController(navController, conf)
+        bind.navView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(conf) || super.onSupportNavigateUp()
     }
 }
